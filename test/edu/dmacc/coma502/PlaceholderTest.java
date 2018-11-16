9a43e9e34af5368c6bbe93df8f466a92b3e491d3
John Coffie <j_coffie@yahoo.com>
2018-11-16 17:28:48 -0600

    add packages

---
+++ test/edu/dmacc/coma502/PlaceholderTest.java
@@ -0,0 +1,14 @@
+package edu.dmacc.coma502;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlaceholderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
